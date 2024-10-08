5effe044e38f92c0b8d4d236cba88b0b4777496f
Renato Moreno Agudo <renatomorenoagudo2@gmail.com>
2024-10-07 21:21:14 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users =[]
+
+app.post('/usuarios',(req, res)=>{
+    console.log(req)
+    users.push()
+    res.send('ok post')
+})
+
+/*HTTP Métodos:
+get=listar
+post=criar
+put=editar varios
+patch=editar um
+delete=deltar
+
+Rotas:
+1-tipo/metodo http
+2-endereço=www.loja...
+*/
+app.get('/usuarios', (req, res)=>{
+    res.send('ok, deu bom')
+})
+
+//porta:
+app.listen(3000)
+
+/*
+-criar nossa API de usuarios
+-criar um usuario
+-listar todos usuarios
+-editar usuarios
+-deletar usuarios
+ */
+//para servidor reiniciar sozinho qdo tiver alteração:
+//node --watch server.js
+
